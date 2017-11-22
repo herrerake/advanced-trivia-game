57dc37c9246f290219464847469b6a94be5f27da
Kevin Herrera <1herrerake@gmail.com>
2017-11-22 16:29:40 -0500

    working game with styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,108 +5,111 @@ $(document).ready(function() {
 
   // Creating an object to hold our questions.
   let questions = [{
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: ""
+    question: "What was Daniela's first car?",
+    answers: ["Honda Civic", "Jeep Wrangler", "Toyota Camry", "Nissan Sentra"],
+    correctAnswer: "Nissan Sentra"
   },
   {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: ""
+    question: "What is Daniela's main dog's name?",
+    answers: ["Reeces", "Snickers", "Stewie", "Lola"],
+    correctAnswer: "Snickers"
   },
   {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: ""
+    question: "Where was Daniela born?",
+    answers: ["Argentina", "Virginia", "Portugal", "Angola"],
+    correctAnswer: "Portugal"
   },
   {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: ""
+    question: "When does Daniela find it okay to NOT pee in a restroom?",
+    answers: ["During a hike", "In an emergency and theres not bathroom in sight", "When the laughing can't stop", "All of the above"],
+    correctAnswer: "All of the above"
   },
   {
-    question: "",
-    answers: ["", "", "", ""],
-    correctAnswer: ""
+    question: "True or False: Did Daniela have braces for 2 years to fix her grill?",
+    answers: ["TRUUUUUE", "False"],
+    correctAnswer: "TRUUUUUE"
+  },
+  {
+    question: "What is a sure fire way to get Daniela's attention and see that million dollar smile?",
+    answers: ["Hey Daniela", "Ayo D", "Excuse me miss", "OYE FEA!"],
+    correctAnswer: "OYE FEA!"
   }];
 
   // variables to hold our set interval and counter
   let timer;
   let countStartNum = 25;
 
-  var viewPort = $("quiz-area");
+  var viewPort = $("#quiz-area");
 
   // creating the game object to hold the functions and variables to the game
   var game = {
-    gameQuestions: questions,
+    questions: questions,
     currentQuestion: 0,
     counter: countStartNum,
     correct: 0,
     incorrect: 0,
 
     countdown: function() {
-      this.counter--;
-      $("#counterNum").text(this.counter);
-      if (this.counter === 0) {
+      game.counter--;
+      $("#counterNum").text(game.counter);
+      if (game.counter === 0) {
         console.log("TIME UP");
-        this.timeUp();
+        game.timeUp();
       }
     },
-    // The bind() method creates a new function that, when called, has its
-    // this keyword set to the provided value, with a given sequence of arguments
-    // preceding any provided when the new function is called.
+
     loadQuestion: function() {
-      timer = setInterval(this.countdown.bind(this), 1000);
+      timer = setInterval(game.countdown, 1000);
 
-      viewPort.html("<h2>" + gameQuestions[this.currentQuestion].question + "</h2>");
+      viewPort.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
 
-      for (var i = 0; i < gameQuestions[this.currentQuestion].answers.length; i++) {
-        viewPort.append("<button class='answer-btn' id='button' data-name='" + gameQuestions[this.currentQuestion].answers[i]
-        + "'>" + gameQuestions[this.currentQuestion].answers[i] + "</button>");
+      for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+        viewPort.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+        + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
       }
     },
 
     nextQuestion: function() {
-      this.counter = window.countStartNum;
-      $("#counter-num").text(this.counter);
-      this.currentQuestion++;
-      this.loadQuestion.bind(this)();
+      game.counter = countStartNum;
+      $("#counterNum").text(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
     },
 
     timeUp: function() {
 
-      clearInterval(window.timer);
+      clearInterval(timer);
 
-      $("#counter-num").text(this.counter);
+      $("#counterNum").html(game.counter);
 
       viewPort.html("<h2>Out of Time!</h2>");
-      viewPort.append("<h3>The Correct Answer Was: " + gameQuestions[this.currentQuestion].correctAnswer);
+      viewPort.append("<h3>The Correct Answer Was: " + questions[this.currentQuestion].correctAnswer);
 
-      if (this.currentQuestion === questions.length - 1) {
-        setTimeout(this.results, 2 * 1000);
+      if (game.currentQuestion === questions.length - 1) {
+        setTimeout(game.results, 3 * 1000);
       }
       else {
-        setTimeout(this.nextQuestion, 2 * 1000);
+        setTimeout(game.nextQuestion, 3 * 1000);
       }
     },
 
     results: function() {
 
-      clearInterval(window.timer);
+      clearInterval(timer);
 
       viewPort.html("<h2>Quiz Complete, here is how you did!</h2>");
 
-      $("#counterNum").text(this.counter);
+      $("#counterNum").text(game.counter);
 
-      viewPort.append("<h3>Correct Answers: " + this.correct + "</h3>");
-      viewPort.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-      viewPort.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+      viewPort.append("<h3>Correct Answers: " + game.correct + "</h3>");
+      viewPort.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+      viewPort.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
       viewPort.append("<br><button id='start-over'>Start Over?</button>");
     },
 
     clicked: function(e) {
-      clearInterval(window.timer);
-      if ($(e.target).attr("data-name") === gameQuestions[this.currentQuestion].correctAnswer) {
+      clearInterval(timer);
+      if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
         this.answeredCorrectly();
       }
       else {
@@ -116,34 +119,34 @@ $(document).ready(function() {
 
     answeredIncorrectly: function() {
 
-      this.incorrect++;
+      game.incorrect++;
 
-      clearInterval(window.timer);
+      clearInterval(timer);
 
-      viewPort.html("<h2>WRONG!</h2>");
-      viewPort.append("<h3>The Correct Answer was: " + gameQuestions[this.currentQuestion].correctAnswer + "</h3>");
+      viewPort.html("<h2 style='color:red'>WRONG!</h2>");
+      viewPort.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
 
-      if (this.currentQuestion === question.length -1) {
-        setTimeout(this.results.bind(this), 2 * 1000);
+      if (game.currentQuestion === questions.length -1) {
+        setTimeout(game.results, 3 * 1000);
       }
       else {
-        setTimeout(this.nextQuestion.bind(this), 2 * 1000)
+        setTimeout(game.nextQuestion, 3 * 1000)
       }
     },
 
     answeredCorrectly: function() {
 
-      clearInterval(window.timer);
+      clearInterval(timer);
 
-      this.correct++;
+      game.correct++;
 
-      viewPort.html("<h2>Correct!</h2>");
+      viewPort.html("<h2 style='color:green'>Correct!</h2>");
 
-      if (this.currentQuestion === questions.length -1) {
-        setTimeout(this.results.bind(this), 2 * 1000);
+      if (game.currentQuestion === questions.length -1) {
+        setTimeout(game.results, 3 * 1000);
       }
       else {
-        setTimeout(this.nextQuestion.bind(this), 2 * 1000);
+        setTimeout(game.nextQuestion, 3 * 1000);
       }
     },
 
@@ -158,14 +161,16 @@ $(document).ready(function() {
 
   // CLICK EVENTS
 
-  $(document).on("click", "#start-over", game.reset.bind(game));
+  $(document).on("click", "#start-over", function() {
+    game.reset();
+  });
 
-  $(document).on('click', ".answer-btn", function(e) {
-    game.clicked.bind(game, e)();
+  $(document).on('click', ".answer-button", function(e) {
+    game.clicked(e);
   });
 
   $(document).on("click", "#start", function() {
-    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-num'>25</span> Seconds</h2>");
-    game.loadQuestion.bind(game)();
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counterNum'>25</span> Seconds</h2>");
+    game.loadQuestion();
   });
 });
